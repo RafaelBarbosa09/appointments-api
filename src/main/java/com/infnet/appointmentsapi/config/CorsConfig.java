9068b89071fef0b7543a019e71f9e1dac25b951d
Rafael Barbosa <rafaeldacostabarbosa@gmail.com>
2023-11-18 16:27:05 -0300

    feat: configura cors no spring security

--- src/main/java/com/infnet/appointmentsapi/config/CorsConfig.java
+++ src/main/java/com/infnet/appointmentsapi/config/CorsConfig.java
@@ -2,16 +2,21 @@ package com.infnet.appointmentsapi.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class CorsConfig implements WebMvcConfigurer {
 
     @Override
-    public void addCorsMappings(CorsRegistry registry) {
+        public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
-                .allowedMethods("GET", "POST", "PUT", "PATCH","DELETE")
-                .allowedHeaders("*");
+                .allowedHeaders("*")
+                .allowedOrigins("*")
+                .exposedHeaders("Authorization", "token", "geracao", "application", "Access-Control-Allow-Methods", "Access-Control-Allow-Headers", "Access-Control-Max-Age", "Access-Control-Request-Headers", "Access-Control-Request-Method")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowCredentials(false)
+                .maxAge(3600);
     }
 }
