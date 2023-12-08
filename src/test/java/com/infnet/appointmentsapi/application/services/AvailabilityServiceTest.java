9a0ff51c685f1bf901adf69994e3698e9250a717
Rafael Barbosa <rafaeldacostabarbosa@gmail.com>
2023-12-08 18:05:39 -0300

    test: inclui testes para availability service

--- src/test/java/com/infnet/appointmentsapi/application/services/AvailabilityServiceTest.java
+++ src/test/java/com/infnet/appointmentsapi/application/services/AvailabilityServiceTest.java
@@ -1,5 +1,7 @@
 package com.infnet.appointmentsapi.application.services;
 
+import com.infnet.appointmentsapi.domain.dto.AvailabilityRequestDTO;
+import com.infnet.appointmentsapi.domain.dto.TimeSlotRequestDTO;
 import com.infnet.appointmentsapi.domain.repositories.AvailabilityRepository;
 import com.infnet.appointmentsapi.domain.repositories.ProfessionalRepository;
 import com.infnet.appointmentsapi.domain.repositories.TimeSlotRepository;
@@ -35,7 +37,7 @@ class AvailabilityServiceTest {
     private AvailabilityService availabilityService;
 
     @Test
-    void testGetAllAvaialabilitiesByDateAndProfessionalIdShouldReturnSuccess() {
+    void testGetAllAvaialabilitiesByDateAndProfessionalId_Success() {
         Mockito.when(availabilityRepository.findAllAvaialabilitiesByDateAndProfessionalId(
                 Mockito.any(LocalDate.class),
                 Mockito.any(Long.class)))
@@ -50,4 +52,37 @@ class AvailabilityServiceTest {
 
     }
 
+    @Test
+    void getAllAvailabilities_Success() {
+        Mockito.when(availabilityRepository.findAll()).thenReturn(List.of(new Availability()));
+
+        assertDoesNotThrow(() -> availabilityService.getAllAvailabilities());
+
+        Mockito.verify(availabilityRepository, Mockito.times(1)).findAll();
+    }
+
+    @Test
+    void createAvailability_Success() {
+        Mockito.when(availabilityRepository.save(Mockito.any(Availability.class))).thenReturn(new Availability());
+
+        assertDoesNotThrow(() -> availabilityService.createAvailability(new AvailabilityRequestDTO(
+                "2023-11-23",
+                new TimeSlotRequestDTO(
+                        "10:00",
+                        "11:00"
+                ),
+                1L
+        )));
+
+        Mockito.verify(availabilityRepository, Mockito.times(1)).save(Mockito.any(Availability.class));
+    }
+
+    @Test
+    void getAvailabilityByDate() {
+        Mockito.when(availabilityRepository.findByDate(Mockito.any(LocalDate.class))).thenReturn(new Availability());
+
+        assertDoesNotThrow(() -> availabilityService.getAvailabilityByDate("2023-11-23"));
+
+        Mockito.verify(availabilityRepository, Mockito.times(1)).findByDate(Mockito.any(LocalDate.class));
+    }
 }
